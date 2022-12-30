32ee3a64b731031756a703957cf037a61fe7725d
Abhishek <abhishekbhikule76@gmail.com>
2022-12-30 21:51:29 +0530

    done

--- Netflix-Landing-Page/script.js
+++ Netflix-Landing-Page/script.js
@@ -1,7 +1,7 @@
 function auth() {
   const email1 = document.getElementById("email1").value;
   const email2 = document.getElementById("email2").value;
-  if (email1 == "admin@gmail.com" || email2 == "admin@gmail.com") {
+  if (email1 == "admin@anything.com" || email2 == "admin@anything.com") {
     window.location.assign("/Netflix-Landing-Page/Netflix-Website/index.html");
     alert("login Succedfull");
   } else {
